65b944734881aa6a144b3c05ee2eb54f5e8bc276
Sadayshi Tada <tadainfra@gmail.com>
2021-01-07 13:08:07 +0900

    Amplify追加

--- react-amplified/src/index.js
+++ react-amplified/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+import { DataStore } from '@aws-amplify/datastore';
+import { Todo } from './models';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +19,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+await DataStore.save(
+  new Todo({
+  "name": "Lorem ipsum dolor sit amet",
+  "description": "Lorem ipsum dolor sit amet"
+})
+);
